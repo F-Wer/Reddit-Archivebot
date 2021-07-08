57d6ea075d51eb6cacbc092e399ff7c2ebed0450
Fabian <fabian99.werner@gmail.com>
2021-07-08 21:19:12 +0200

    Bug fixes and improved performance drasticly by directyl accessing the outline api

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
 import waybackpy
-import os
+import urllib.parse
+import requests
+import json
+from praw.models import MoreComments
+outline = 'https://outline.com/'
 
 
 class Reddit:
 
     def get_submissions(self):
+        """[summary]
+        Besorgt sich alle Submissions, die noch nicht archived sind, eines spezifischen Subreddits und schaut welche per outline und welche archive.org archivierbar sind.
+        Link Posts ==> outline
+        Rest ==> archive.org
+        """
         not_supported = ['nytimes.com', 'wsj.com',
                          'redd.it', 'youtube.com', 'youtu.be', 'imgur.com']
         for submission in reddit.subreddit("TESTFORABOT01").stream.submissions():
-            # time.sleep(2)
-            print(submission.title)
+            time.sleep(2)
             if submission.__dict__.get('post_hint', None) == 'link':
                 if submission.archived != True:
                     if any([x in submission.url for x in not_supported]):
@@ -23,32 +31,62 @@ class Reddit:
                         currenturl = 'Diese URL wird bei Outline nicht unterstützt'
                     else:
                         if submission.url in "outline.com":
-                            print("Outline Link")
                             currenturl = submission.url
                         else:
-                            print('URL wird unterstützt' + ' ' + submission.url)
+                            print('URL wird unterstützt' +
+                                  ' ' + submission.url)
                             currenturl = self.create_outline_url(
-                                submission.url, submission.title)
-                            print(str(submission.title) + ' ' + str(submission.url))
-                    print(str(submission.title) + ' ' + str(submission.url))
+                                submission.url)
                     archiveurl = self.create_archive_url(
-                    submission.url, submission.title)
+                        submission.url, submission.title)
                     self.comment(currenturl, archiveurl, submission.id)
 
-    def create_outline_url(self, url, title):
-        outline = 'https://outline.com/'
+    def create_outline_url_old(self, url):
+        start_old = time.time()
+        """[summary]
+            Läuft momentan über Selenium mit Firefox, da meine Pathvariables nicht wirklich funktionieren, ist dies über env variables gelöst.
+            Da Chrome manchmal random abgestützt ist, ist nur Firefox implementiert.
+
+            Dauert ungefähr 15 sekunden abzulaufen
+        Args:
+            url ([url]): Input für die URL um diese an Outline weiterzugeben
+            title ([str]): Der Titel des Posts um diesen
+
+        Returns:
+            [url]: Outlineurl
+        """
         concaturl = outline + url
         options = Options()
-        # if random.randint(0, 1) == 0:
         options.headless = True
         driver = webdriver.Firefox(
             executable_path=config.geckodriver, options=options)
         driver.get(concaturl)
-        time.sleep(10)
+        time.sleep(5)
         if concaturl != driver.current_url:
             time.sleep(2)
             outlineurl = driver.current_url
             driver.close()
+        end_old = time.time()
+        print(end_old - start_old)
+        return outlineurl
+
+    def create_outline_url(self, url):
+        """[summary]
+        Erstellen von Outline Links über direkten API Zugriff.
+        Dauert <1 Sekunde
+        Args:
+            url ([url]): Input für die URL um diese an Outline weiterzugeben
+
+        Returns:
+             [url]: Outlineurl
+        """
+        outline_api = "https://api.outline.com/v3/parse_article?source_url="
+        escapedinputurl = urllib.parse.quote_plus(url)
+        response = requests.get(outline_api + escapedinputurl)
+        textresponse = json.dumps(response.json(), indent=4, sort_keys=True)
+        resp = json.loads(textresponse)  # Beispiel json in data.json
+        short_code = (resp['data']['short_code'])
+        outlineurl = outline + short_code
         return outlineurl
 
     def create_archive_url(self, url, title):
@@ -58,22 +96,44 @@ class Reddit:
             wayback = waybackpy.Url(url, user_agent)
             archive = wayback.save()
             archiveurl = archive.archive_url
-            print(archiveurl)
             return archiveurl
         except Exception as e:
             print(e)
             archiveurl = ' dieser Link kann nicht archiviert werden, da er auf eine andere Seite weiterleitet!'
             return archiveurl
 
-    def comment(self, link1, link2, id):
-        reply_template = "Dies ist ein Bot für die Erstellung von Outlinelinks " + str(link1) + " und Archive Links" + str(link2)
-        submission = reddit.submission(id=id)
-        submission.reply(reply_template)
-        print("Kommentar wurde gepostet" + ' ' +  "https://old.reddit.com/r/TESTFORABOT01/comments/" + id)
-        # reddit.subreddit("TESTFORABOT01").submit(title, url=link)
+    def comment(self, outline, archive, id):
+        """[summary]
+            Kommentiert einen Link zu Outline und Archive.org und zwar nur, wenn zuvor noch nicht kommentiert wurde.
+        Args:
+            outline ([url]): URL von Outline
+            archive ([url]): URL von Archive
+            id ([submission.id]): Submission ID um einen Post zu bekommen
+        """
+        submissions = reddit.submission(id=id)
+        for comment in submissions.comments:
+            # https://praw.readthedocs.io/en/latest/tutorials/comments.html#extracting-comments-with-praw
+            if isinstance(comment, MoreComments):
+                continue
+            if comment.author == config.botname:
+                pass
+            else:
+                try:
+                    reply_template = "Dies ist ein Bot für die Erstellung von Outlinelinks " + \
+                        str(outline) + " und Archive Links " + str(archive)
+                    submission = reddit.submission(id=id)
+                    submission.reply(reply_template)
+                    print("Kommentar wurde gepostet" + ' ' +
+                          "https://old.reddit.com/r/TESTFORABOT01/comments/" + id)
+                except Exception as e:
+                    print(e)
+                    pass
 
 
 if __name__ == "__main__":
+    """[summary]
+    Instanz der Klasse erzeugen und Config Daten laden
+    """
     try:
         r = Reddit()
         reddit = praw.Reddit(
@@ -86,7 +146,3 @@ if __name__ == "__main__":
         r.get_submissions()
     except Exception as e:
         print(e)
-        if os.name == "nt":
-            os.system("taskkill /f /im firefox.exe")
-        else:
-            os.system("killall firefox")
